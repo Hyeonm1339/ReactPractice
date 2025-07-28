53e4a67569496a28f4173c33f7d95c2e5c8bda66
Hyeonm1339 <jhjimess@naver.com>
2025-07-28 20:00:14 +0900

    13강 리액트 최적화 강의까지 수강

--- 12 Behind the Scenes/vite.config.js
+++ 12 Behind the Scenes/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as MillionLint from "million/compiler";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [MillionLint.vite({auto:true}),react()],
 })
