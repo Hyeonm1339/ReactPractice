17215ec8d7023028ee53d828d8ec429e0b0da961
Hyeonm1339 <jhjimess@naver.com>
2025-08-21 19:24:15 +0900

    리액트 라우터 기본 강의 수강. (라우터의 기초)

---
+++ 22 ReactRouter Start/src/pages/Error.js
@@ -0,0 +1,12 @@
+import MainNavigation from "../components/MainNavigation";
+
+function ErrorPage() {
+    return <>
+        <MainNavigation/>
+        <main>
+            <h1>An error </h1>
+        </main>
+    </>
+}
+
+export default ErrorPage;
