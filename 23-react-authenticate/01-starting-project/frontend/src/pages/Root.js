eb0607b7389b6eedba5cadbf429bf04ee0f8add8
HYEONM1339 <jhjimess@naver.com>
2025-10-05 19:42:16 +0900

    localstorage를 활용한 기본 로그인, 로그아웃 처리 코드 추가 ( loader를 활용한 토큰인증,)

--- 23-react-authenticate/01-starting-project/frontend/src/pages/Root.js
+++ 23-react-authenticate/01-starting-project/frontend/src/pages/Root.js
@@ -1,19 +1,38 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import {Outlet, useLoaderData, useSubmit} from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import {useEffect} from "react";
+import {getTokenDuration} from "../util/auth";
 
 function RootLayout() {
-  // const navigation = useNavigation();
+    const token = useLoaderData()
+    const submit = useSubmit()
+    // const navigation = useNavigation();
+    //토큰이 없다면 무시, 토큰이 있다면 1시간이 지난경우, 자동 로그아웃처리를 진행.
+    useEffect(() => {
+        if (!token) {
+            return;
+        }
+        if (token === 'EXPIRED') {
+            submit(null, {action: '/logout', method: 'POST'})
+            return ;
+        }
+        const tokenDuration = getTokenDuration();
+        
+        setTimeout(() => {
+            submit(null, {action: '/logout', method: 'POST'})
+        }, tokenDuration)
+    }, [token, submit]);
 
-  return (
-    <>
-      <MainNavigation />
-      <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <MainNavigation/>
+            <main>
+                {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+                <Outlet/>
+            </main>
+        </>
+    );
 }
 
 export default RootLayout;
