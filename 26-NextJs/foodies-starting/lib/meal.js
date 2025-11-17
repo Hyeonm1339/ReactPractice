a7ea34b922a7c83f540310359fe0100bf0813e36
Hyeonm1339 <jhjimess@naver.com>
2025-11-17 15:07:27 +0900

    페이지 세부데이터를 DB에서 조회후. 기본페이지 구성기능 추가.

--- 26-NextJs/foodies-starting/lib/meal.js
+++ 26-NextJs/foodies-starting/lib/meal.js
@@ -4,9 +4,14 @@ const db = sql('meals.db');
 
 export async function getMeals() {
 
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    // await new Promise((resolve) => setTimeout(resolve, 2000))
 
     // throw new Error('Loading meals failed');
     //run()은 데이터를 insert,update할때 사용하고, ALL은 조회용도로 사용한다.
     return db.prepare('SELECT * FROM meals').all();
+}
+
+export function getMeal(slug){
+    //SQL 인젝션 방지를 위해 = ' + slug가 아닌, get()을 통해서 파라미터를 전달한다.
+    return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
