ef07557bf115918d1944e993ce863336699c85fc
Hyeonm1339 <jhjimess@naver.com>
2025-11-17 09:08:52 +0900

    Next.js 강의 진행, 헤더분리, css모듈화 까지 작업

---
+++ 26-NextJs/foodies-starting/app/page.js
@@ -0,0 +1,11 @@
+import classes from './page.module.css'
+
+export default function Home() {
+    return (
+        <>
+            <header className={classes.header}>
+            </header>
+            <main></main>
+        </>
+    );
+}
